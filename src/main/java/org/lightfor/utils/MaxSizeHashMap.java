dd1b1b7ff19bd228330e8863f61b54b601f664d8
lightfor <death_light@foxmail.com>
2016-09-12 20:23:24 +0800

    MaxSizeHashMap, not thread safe

---
+++ src/main/java/org/lightfor/utils/MaxSizeHashMap.java
@@ -0,0 +1,21 @@
+package org.lightfor.utils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 有最大值限制的HashMap
+ * Created by Light on 2016/9/6.
+ */
+public class MaxSizeHashMap<K, V> extends LinkedHashMap<K, V> {
+    private  final int maxSize;
+
+    public MaxSizeHashMap(int maxSize){
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    protected  boolean removeEldestEntry(Map.Entry<K,V> eldest){
+        return size() > maxSize;
+    }
+}
