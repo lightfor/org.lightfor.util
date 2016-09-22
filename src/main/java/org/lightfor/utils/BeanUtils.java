177a4de13ede9c01eed03b3d343376c07441b361
lightfor <death_light@foxmail.com>
2016-09-22 18:43:24 +0800

    BeanUtils TODO more useful methods

---
+++ src/main/java/org/lightfor/utils/BeanUtils.java
@@ -0,0 +1,31 @@
+package org.lightfor.utils;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+
+/**
+ * bean工具类
+ * Created by Light on 2016/7/21.
+ */
+public class BeanUtils {
+    public static Method getWriteMethod(Object beanObj, String name){
+        Method method = null;
+        try {
+            BeanInfo info = Introspector.getBeanInfo(beanObj.getClass());
+            PropertyDescriptor[] pds = info.getPropertyDescriptors();
+            if(pds != null){
+                for (PropertyDescriptor pd : pds) {
+                    String pName = pd.getName();
+                    if(name.equals(pName)){
+                        method = pd.getWriteMethod();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return method;
+    }
+}
