182a81e5a214c295b2f8a5ad0f4806f1d7959ac7
lightfor <death_light@foxmail.com>
2017-09-02 21:47:25 +0800

    test performance

--- src/test/java/org/lightfor/util/DBUtilsTest.java
+++ src/test/java/org/lightfor/util/DBUtilsTest.java
@@ -2,6 +2,9 @@ package org.lightfor.util;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * db utils test
  * Created by Light on 2017/8/21.
@@ -67,7 +70,8 @@ public class DBUtilsTest {
 
     @Test
     public void test4(){
-        DBUtils.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/test", "dev", "dev");
+        //DBUtils.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/test", "dev", "dev");
+        DBUtils.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:32783/dev", "dev", "dev");
         long startTime = 0;
         DBUtils.setAutoCommit(false);
         for(int i = 0 ; i < Integer.MAX_VALUE ; i++){
@@ -82,10 +86,11 @@ public class DBUtilsTest {
             DBUtils.insert("insert into forum (name, parent_id) values ('11',10)");
             DBUtils.insert("insert into forum (name, parent_id) values ('11',10)");
             DBUtils.insert("insert into forum (name, parent_id) values ('11',10)");
-            DBUtils.update("update forum set name='root1' where id=2");
-            DBUtils.update("update forum set name='root2' where id=2");
-            DBUtils.commit();
+            //DBUtils.update("update forum set name='root1' where id=2");
+            //DBUtils.update("update forum set name='root2' where id=2");
+
             if(i % 250 == 249){
+                DBUtils.commit();
                 System.out.println(System.currentTimeMillis() - startTime);
             }
         }
@@ -101,6 +106,40 @@ public class DBUtilsTest {
                 startTime = System.currentTimeMillis();
             }
             DBUtils.selectOne("select * from forum where id = 2");
+            if(i % 250 == 249){
+                System.out.println(System.currentTimeMillis() - startTime);
+            }
+        }
+    }
+
+
+    @Test
+    public void test6(){
+        DBUtils.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:32779/dev", "dev", "dev");
+        long startTime = 0;
+        DBUtils.setAutoCommit(false);
+        for(int i = 0 ; i < Integer.MAX_VALUE ; i++){
+            if(i % 250 == 0){
+                startTime = System.currentTimeMillis();
+            }
+            DBUtils.insert("insert into forum (name, parent_id) values ('"+i+"', "+i+")" );
+            if(i % 250 == 249){
+                System.out.println(System.currentTimeMillis() - startTime);
+            }
+        }
+    }
+
+    @Test
+    // 2002 no index
+    // 150 index
+    public void test7(){
+        DBUtils.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:32779/dev", "dev", "dev");
+        long startTime = 0;
+        for(int i = 0 ; i < Integer.MAX_VALUE ; i++){
+            if(i % 250 == 0){
+                startTime = System.currentTimeMillis();
+            }
+            DBUtils.selectOne("select * from forum where name='"+i+"'" );
             if(i % 250 == 249){
                 System.out.println(System.currentTimeMillis() - startTime);
             }
