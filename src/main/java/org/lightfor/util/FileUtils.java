48970b43854fe3bc019e4ba5fde1a90259f7bb50
lightfor <death_light@foxmail.com>
2018-02-05 17:36:29 +0800

    add count pattern, fix jdk 8 reading file

--- src/main/java/org/lightfor/util/FileUtils.java
+++ src/main/java/org/lightfor/util/FileUtils.java
@@ -1,8 +1,5 @@
 package org.lightfor.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,10 +10,14 @@ import java.nio.file.Paths;
 
 /**
  * 文件工具类
- * Created by Light on 2016/8/11.
+ * @author Light
+ * @date 2016/8/11.
  */
 public enum  FileUtils {
+    /** 实例 */
     INSTANCE;
+
+    public static final String NEW_LINE = "\n";
 
     public static String fileToStringJDK6elow(String filePath){
         InputStream is = null;
@@ -29,7 +30,7 @@ public enum  FileUtils {
             StringBuilder sb = new StringBuilder();
 
             while(line != null) {
-                sb.append(line).append("\n");
+                sb.append(line).append(NEW_LINE);
                 line = br.readLine();
             }
             return sb.toString();
@@ -97,14 +98,15 @@ public enum  FileUtils {
         }
     }
 
-/*    public String fileToStringJDK8(String filePath){
+    public static String fileToStringJDK8(String filePath){
         try {
-            Files.lines(Paths.get(filePath)).forEach(System.out::println);
-            return null;
+            StringBuilder sb = new StringBuilder();
+            Files.lines(Paths.get(filePath)).forEach(s -> sb.append(s).append(NEW_LINE));
+            return sb.toString();
         } catch (Exception e) {
             return null;
         }
-    }*/
+    }
 
     public static boolean deleteFile(String fileName){
         File file = new File(fileName);
@@ -136,6 +138,27 @@ public enum  FileUtils {
             }
         }
         return flag && dirFile.delete();
+    }
+
+    public static int countPattern(String path, String pattern) {
+        File pathFile = new File(path);
+        if(!pathFile.exists() || !pathFile.isDirectory()){
+            return 0;
+        }
+        File[] files = pathFile.listFiles();
+        int result = 0;
+        if(files != null){
+            for(File file : files){
+                if(file.isFile()){
+                    if(file.getName().matches(pattern)){
+                        result++;
+                    }
+                } else if(file.isDirectory()){
+                    result += countPattern(file.getAbsolutePath(), pattern);
+                }
+            }
+        }
+        return result;
     }
 
 }
